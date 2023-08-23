4ecec4102486d22c96392663005ff70eae18bd68
Fábio Júnior <fjrleao@gmail.com>
2023-08-22 23:38:07 -0300

    test: return appropriated status code when error

--- backend/tests/users_test.py
+++ backend/tests/users_test.py
@@ -21,3 +21,53 @@ def test_user_success_register(client):
     assert response.json["address"]["number"] == 'valid_number'
     assert response.json["address"]["country"] == 'valid_country'
     assert response.json["address"]["city"] == 'valid_city'
+
+
+def test_wrong_key_user_data(client):
+    user_data = {
+        'name': 'valid_name',
+        'email_wrong': 'valid_email',
+        'address': {
+            'street': 'valid_street',
+            'zip_code': 'valid_zip_code',
+            'number': 'valid_number',
+            'country': 'valid_country',
+            'city': 'valid_city'
+        }
+    }
+
+    response = client.post('/users', json=user_data)
+    assert response.status_code == 400
+
+
+def test_missing_key_user_data(client):
+    user_data = {
+        'name': 'valid_name',
+        'address': {
+            'street': 'valid_street',
+            'zip_code': 'valid_zip_code',
+            'number': 'valid_number',
+            'country': 'valid_country',
+            'city': 'valid_city'
+        }
+    }
+
+    response = client.post('/users', json=user_data)
+    assert response.status_code == 400
+
+
+def test_user_data_already_exists(client):
+    user_data = {
+        'name': 'valid_name',
+        'email': 'valid_email',
+        'address': {
+            'street': 'valid_street',
+            'zip_code': 'valid_zip_code',
+            'number': 'valid_number',
+            'country': 'valid_country',
+            'city': 'valid_city'
+        }
+    }
+
+    response = client.post('/users', json=user_data)
+    assert response.status_code == 409
