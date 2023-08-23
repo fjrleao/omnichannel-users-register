a7f171d046ee5b32863d46856413c54fb59296dd
Fábio Júnior <fjrleao@gmail.com>
2023-08-23 10:50:11 -0300

    feat: validate zip_code in viacep api

--- backend/app/controllers/users_controllers.py
+++ backend/app/controllers/users_controllers.py
@@ -1,15 +1,27 @@
+from os import environ
 from http import HTTPStatus
+import requests
 from flask import request, json, jsonify
 from mongoengine import FieldDoesNotExist, ValidationError, NotUniqueError
 from pymongo.errors import DuplicateKeyError
 
 from app.models.users_models import User
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def create_user():
     try:
 
         data = request.get_json()
+
+        valid_zip_code = requests.get(
+            f'{environ.get("VIACEP_BASE_URL")}/{data["address"]["zip_code"]}/json'
+        )
+
+        if valid_zip_code.status_code == 400:
+            return jsonify({'message': 'Invalid address'}), HTTPStatus.BAD_REQUEST
 
         new_user = User(**data)
         new_user.save()
