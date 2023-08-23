e1ff25002d8469ab1e1c28aecb656e42267d6523
Fábio Júnior <fjrleao@gmail.com>
2023-08-23 15:55:01 -0300

    chore: CORS app

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,5 +1,6 @@
 from os import environ
 from flask import Flask
+from flask_cors import CORS
 from mongoengine import connect
 from dotenv import load_dotenv
 
@@ -11,9 +12,11 @@ load_dotenv()
 def create_app():
 
     app = Flask(__name__)
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     connect(host=environ.get('MONGODB_URI'))
 
+    CORS(bp_user)
     app.register_blueprint(bp_user)
 
     return app
