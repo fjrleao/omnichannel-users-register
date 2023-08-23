f7a4936ad17d7c4cacbcfb9f27be6212d199ec6d
Fábio Júnior <fjrleao@gmail.com>
2023-08-23 11:46:50 -0300

    feat: FormContainer e InputForm components

---
+++ frontend/src/components/Form/Container/styles.tsx
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components'
+
+export const SFormContainer = styled.div`
+	display: flex;
+	align-items: center;
+	flex-wrap: wrap;
+	max-width: 46vw;
+	background: #fff;
+	padding: 15px 35px;
+	border-radius: 10px;
+`
