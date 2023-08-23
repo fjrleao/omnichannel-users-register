a85c7ce25e87df8698b82474b9c56951a3bc29fc
Fábio Júnior <fjrleao@gmail.com>
2023-08-22 21:51:21 -0300

    chore: config mongodb connection

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,10 +1,18 @@
+from os import environ
 from flask import Flask
+from mongoengine import connect
+from dotenv import load_dotenv
+
 from app.routes.users_routes import bp_user
+
+load_dotenv()
 
 
 def create_app():
 
     app = Flask(__name__)
+
+    connect(host=environ.get('MONGODB_URI'))
 
     app.register_blueprint(bp_user)
 
